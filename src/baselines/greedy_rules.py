280c72045ef539b0a3ac98bc9cfdbe2f426e33d4
marksverdhei <marksverdhei@hotmail.com>
2026-01-28 12:42:06 +0100

    update code

--- src/baselines/greedy_rules.py
+++ src/baselines/greedy_rules.py
@@ -2,67 +2,68 @@
 Browser environment for Pokemon game
 """
 import nest_asyncio
+from src.envs.game_env import PokemonBrowserEnv
 nest_asyncio.apply()
 
 
+class GreedyRulesBaseline:
+    def __init__(self, env) -> None:
+        self.env = env
 
-def navigate_title_screen():
-    ocr_data = env.ocr()
-    
-    if any('continue' in s for s in ocr_data):
-        navigate_titlescreen_continue()
-    else:
-        navigate_titlescreen_newgame()
+    def navigate_title_screen(self):
+        ocr_data = self.env.ocr()
+        if any('continue' in s for s in ocr_data):
+            self.navigate_titlescreen_continue()
+        else:
+            self.navigate_titlescreen_newgame()
 
-def navigate_titlescreen_newgame():
-    print("New game")
-    env.send_action("a")
-    env.send_action("a")
-    navigate_pc()
+    def navigate_titlescreen_newgame(self):
+        print("New game")
+        self.env.send_action("a")
+        self.env.send_action("a")
+        self.navigate_pc()
 
-def navigate_titlescreen_continue():
-    print("Continue")
-    env.send_action("a")
-    env.send_action("a")
+    def navigate_titlescreen_continue(self):
+        print("Continue")
+        self.env.send_action("a")
+        self.env.send_action("a")
+
+    def navigate_pc(self):
+        # PC
+        # Random pokemon
+        self.env.send_action("left")
+
+        for _ in range(6):
+            self.env.send_action("a")
+
+        # enter game
+        self.env.send_action("up")
+        self.env.send_action("up")
+        self.env.send_action("a")
+
+    def autoplay(self):
+        self.navigate_title_screen()
+        ocr_every = 1
+        i = 0
+
+        while True:
+            if i % ocr_every == 0:
+                ocr_data = self.env.ocr()
+                print(ocr_data)
+                # Cover moves - 'It won't have any effect.'
+                if any("effect" in s for s in ocr_data):
+                    self.env.send_action("down")
+
+            self.env.send_action("a")
+            i += 1
 
 
-def navigate_pc():
-    # PC
-    # Random pokemon
-    env.send_action("left")
-
-    for i in range(6):
-        env.send_action("a")
-
-    # enter game
-    env.send_action("up")
-    env.send_action("up")
-
-    env.send_action("a")
-
-def autoplay():
-    navigate_title_screen()
-    ocr_every = 1
-    i = 0
-
-    while True:
-        if i % ocr_every == 0:
-            ocr_data = env.ocr()
-            print(ocr_data)
-            # Cover moves - 'It won't have any effect.'
-            if any("effect" in s for s in ocr_data):
-                env.send_action("down")
-
-        env.send_action("a")
-
-        i += 1
-
-    
 def main():
     env = PokemonBrowserEnv(image_resolution=(640, 360))
     env.connect()
+    agent = GreedyRulesBaseline(env)
     try:
-        autoplay()
+        agent.autoplay()
     except KeyboardInterrupt:
         print("Terminating")
         env.close()
